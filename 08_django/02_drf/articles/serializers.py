14841ff766aa72b773dd7cc8570b17733ceb4210
moonthree <fpdhslr0725@gmail.com>
2022-10-18 09:26:08 +0900

    a

--- 08_django/02_drf/articles/serializers.py
+++ 08_django/02_drf/articles/serializers.py
@@ -1,9 +1,7 @@
-# rest_framework = drf 이름
 from rest_framework import serializers
-from .models import Article
+from .models import Article, Comment
 
-# 이름에 리스트가 들어가는 이유는
-# 이 클래스는 게시글의 목록(쿼리셋)의 json을 줄 것이므로
+
 class ArticleListSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -11,9 +9,22 @@ class ArticleListSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'content',)
 
 
-# 단일 게시글 정보를 담은 json을 줄 것
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = ('article',)
+
+
 class ArticleSerializer(serializers.ModelSerializer):
+    # comment_set = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    comment_set = CommentSerializer(many=True, read_only=True)
+    comment_count = serializers.IntegerField(source='comment_set.count', read_only=True)
 
     class Meta:
         model = Article
         fields = '__all__'
+
+
+
--- 08_django/02_drf/articles/views.py
+++ 08_django/02_drf/articles/views.py
@@ -2,18 +2,19 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-from .models import Article
-from .serializers import ArticleListSerializer, ArticleSerializer
-# model에서 Article import
-# serializers에서 ArticleListSerializer import
 
-# DRF에선 데코레이터가 필수적임
+from django.shortcuts import get_object_or_404, get_list_or_404
+
+from .serializers import ArticleListSerializer, ArticleSerializer, CommentSerializer
+from .models import Article, Comment
+
+#DRF에선 데코레이터가 필수적임
 @api_view(['GET', 'POST'])
 def article_list(request):
     # GET
     if request.method == 'GET':
-        articles = Article.objects.all()
-
+        #articles = Article.objects.all()
+        articles = get_list_or_404(Article)
         # (articles, many=True)
         # ArticleListSerializer괄호 안에 serializer될 객체 'articles' 넣음
         # 그런데 단일객체가 아니니까 many=True
@@ -32,22 +33,62 @@ def article_list(request):
         # save 실패시 400 리턴 -> raise_exception=True로 대체
         #return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-@api_view(['GET'])
+@api_view(['GET', 'DELETE', 'PUT'])
 def article_detail(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
-    serializer = ArticleSerializer(article)
-    return Response(serializer.data)
+    # article = Article.objects.get(pk=article_pk)
+    article = get_object_or_404(Article, pk=article_pk)
 
-
-@api_view(['GET', 'DELETE'])
-def article_delete(request, article_pk):
-    article = Article.objects.get(pk=article_pk)
     if request.method == 'GET':
         serializer = ArticleSerializer(article)
         return Response(serializer.data)
+    
     elif request.method == 'DELETE':
         article.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+    elif request.method == 'PUT':
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
 
+
+@api_view(['GET'])
+def comment_list(request):
+    if request.method == 'GET':
+        # comments = Comment.objects.all()
+        comments = get_list_or_404(Comment)
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def comment_detail(request, comment_pk):
+    # comment = Comment.objects.get(pk=comment_pk)
+    comment = get_object_or_404(Comment, pk=comment_pk)
+
+    if request.method == 'GET':
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    elif request.method == 'PUT':
+        serializer = CommentSerializer(comment, data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+    
+
+
+@api_view(['POST'])
+def comment_create(request, article_pk):
+    # article = Article.objects.get(pk=article_pk)
+    article = get_object_or_404(Article, pk=article_pk)
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(article=article)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
